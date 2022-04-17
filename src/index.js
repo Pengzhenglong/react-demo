a95309795f885bfec2a2419ba8e19dc644c15440
H.M.E <1316126217@qq.com>
2022-04-17 23:11:51 +0800

    基本功能实现了

--- src/index.js
+++ src/index.js
@@ -1,27 +1,89 @@
 import ReactDOM from "react-dom";
 // import App from './App1'
 import React from "react"
-class Square extends React.Component {
+// class Square extends React.Component {
+//   // constructor(props) {
+//   //   super(props);
+//   //   this.state = {
+//   //     value: null,
+//   //   }
+//   // }
+//   render() {
+//     return (
+//       <button className="square" onClick={() => this.props.onClick()} >
+//         {this.props.value}
+//       </button >
+//     )
+//   }
+// }
+
+// 函数式组件
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
+}
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: null,
+      squares: Array(9).fill(null),
+      xIsNext: true,
     }
   }
-  render() {
-    return (
-      <button className="square" onClick={() => this.setState({ value: 'X' })} >
-        {this.state.value}
-      </button >
-    )
+
+
+  handleClick(i) {
+    // slice拷贝一份数组
+    const squares = this.state.squares.slice()
+    if (calculateWinner(squares) || squares[i]) {
+      return
+    }
+    console.log(squares);
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    // squares[i] = 'X'
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    })
+    console.log(squares)
+
   }
-}
-class Board extends React.Component {
   renderSquare(i) {
-    return <Square value={i} />
+    return <Square value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+    />
   }
   render() {
-    const status = 'Next player: X'
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'winner' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
     return (
       <div>
         <div className="status">
